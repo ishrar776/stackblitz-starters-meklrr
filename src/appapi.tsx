149c596bdeb7bc0b6935fd0f1b8b225074c90387
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-26 09:34:07 +0530

    1 file modified

--- src/appapi.tsx
+++ src/appapi.tsx
@@ -1,10 +1,17 @@
-import Axios from 'axios';
-
-export default Axios.create({
-  baseURL: '../images',
-});
-
-export const allProduct = async () => {
-  const response = await Axios.get('../images/data.json');
-  return response.data;
+// import Axios from 'axios';
+// export default Axios.create({
+//   baseURL: '../images',
+// });
+// export const allProduct = async () => {
+//   const response = await Axios.get('../images/data.json');
+//   return response.data;
+// };
+import React, { useEffect } from 'react';
+export const allProduct = () => {
+  const url = '../images/data.json';
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
+  }, []);
 };
