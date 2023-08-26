7680a4fd59977c25181306d6cd5254e8bb4ac588
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-26 14:27:21 +0530

    1 file modified

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,15 +1,8 @@
-// import { createStore, applyMiddleware } from 'redux';
-// import rootReducer from './reducer/index';
-// import createSagaMiddleware from 'redux-saga';
-// import rootSaga from './rootSaga';
-// const sagaMiddleware = createSagaMiddleware();
-// const store: any = createStore(rootReducer, applyMiddleware(sagaMiddleware));
-// sagaMiddleware.run(rootSaga);
-// export default store;
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counter/CounterSlice';
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducer/index';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './rootSaga';
+const sagaMiddleware = createSagaMiddleware();
+const store: any = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
+export default store;
